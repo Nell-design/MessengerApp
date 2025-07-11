4e8bd648d066f273c18e7f461861fd9da8a6bd4c
Mathieu Adimi <Mathieu.Adimi@chem.loc>
2025-07-11 11:14:17 +0200

    commit

---
+++ app/Events/UserStatusEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class UserStatusEvent implements ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public $userId;
+    public $isOnline;
+
+    public function __construct($userId, $isOnline)
+    {
+        $this->userId = $userId;
+        $this->isOnline = $isOnline;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('user-status');
+    }
+}
